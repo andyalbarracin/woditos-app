91a2a8166d7f0bcae70888260738004194bda935
Andres <andy@MacBook-Air-de-Andres.local>
2026-03-26 16:42:40 -0300

    chore: update capacitor config and package-lock

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.bd305a17b5e548f7a935d8cd989049ea',
-  appName: 'Woditos',
-  webDir: 'dist',
-  server: {
-    url: 'https://bd305a17-b5e5-48f7-a935-d8cd989049ea.lovableproject.com?forceHideBadge=true',
-    cleartext: true,
-  },
+  appId: 'com.woditos.app',
+  appName: 'Wodito',
+  webDir: 'dist'
 };
 
 export default config;
